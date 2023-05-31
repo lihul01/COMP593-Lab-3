c7c9d0cc5aa26d5df35a617a3ab0fed5ccb68524
lihul01 <lihul@flemingcollege.ca>
2023-05-31 16:37:02 -0400

    Update process_sales_data.py

--- process_sales_data.py
+++ process_sales_data.py
@@ -12,6 +12,7 @@ import pandas as pd
 import sys
 import os.path
 from datetime import date
+import re
 
 def main():
     sales_csv_path = get_sales_csv_path()
@@ -67,15 +68,38 @@ def process_sales_data(sales_csv_path, orders_dir_path):
         sales_csv_path (str): Path of sales data CSV file
         orders_dir_path (str): Path of orders directory
     """
-    # TODO: Import the sales data from the CSV file into a DataFrame
-    # TODO: Insert a new "TOTAL PRICE" column into the DataFrame
-    # TODO: Remove columns from the DataFrame that are not needed
-    # TODO: Groups orders by ID and iterate 
-        # TODO: Remove the 'ORDER ID' column
-        # TODO: Sort the items by item number
-        # TODO: Append a "GRAND TOTAL" row
-        # TODO: Determine the file name and full path of the Excel sheet
-        # TODO: Export the data to an Excel sheet
+    # Import the sales data from the CSV file into a DataFrame
+    df = pd.read_csv(sales_csv_path)
+
+    # Insert a new "TOTAL PRICE" column into the DataFrame
+    df.insert(7, 'TOTAL PRICE', df['ITEM QUANTITY'] * df ['ITEM PRICE'])
+
+    # Remove columns from the DataFrame that are not needed
+    df.drop(columns = ['ADDRESS', 'CITY', 'STATE', 'POSTAL CODE', 'COUNTRY'], inplace = True)
+
+    # Groups orders by ID and iterate
+    for order_id, order_df in df.groupby('ORDER ID'):
+
+        # Remove the 'ORDER ID' column
+        order_df.drop(columns = ['ORDER ID'], inplace = True)
+
+        #  Sort the items by item number
+        order_df.sort_values(by = 'ITEM NUMBER', inplace = True)
+
+        # Append a "GRAND TOTAL" row
+        grand_total = order_df['TOTAL PRICE'].sum()
+        grand_total_df = pd.DataFrame({'ITEM PRICE': ['GRAND TOTAL'], 'TOTAL PRICE': [grand_total]})
+        order_df = pd.concat(order_df, grand_total_df)
+
+        # Determine the file name and full path of the Excel sheet
+        customer_name = order_df['CUSTOMER NAME'].values[0]
+        customer_name = re.sub(r'\W', '', customer_name)
+        order_file = f"Order{order_id}_{customer_name}.xlsx"
+        order_excel_path = os.path.join(orders_dir_path, order_file)
+
+        # Export the data to an Excel sheet
+        
+
         # TODO: Format the Excel sheet
         # TODO: Define format for the money columns
         # TODO: Format each colunm
